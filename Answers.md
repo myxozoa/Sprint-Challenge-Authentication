7a49a641603f41bfe82fd7aa82aa4c48a1afe4a6
Ronnie Miksch <fakepreferences@gmail.com>
2018-03-23 09:20:00 -0700

    Completed Answers and Basic Auth functionality, moving to stretch

--- Answers.md
+++ Answers.md
@@ -1,5 +1,14 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
-1.  What does bcrypt do in order to prevent attacks?
-1.  What are the three parts of the JSON Web Token?
+---
+Middleware is software that sits in the middle between two other disparate pieces of software and often facilitates a communication between them, or applies a set of rules.  Sessions are user logins persisted over time (with cookies), bcrypt is a popular encryption/hashing algorithm and library, and JWTs are JSON Web Tokens which are tokens that can be used as an alternative to session cookies.
+
+2.  What does bcrypt do in order to prevent attacks?
+---
+It hashes passwords, so that they can't be known even if the database were compromised.
+And can encrypt data.
+
+3.  What are the three parts of the JSON Web Token?
+---
+The header, payload and signature
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -8,6 +8,16 @@ const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
+  username: {
+    type: String,
+    required: [true, 'Please provide a username and password'],
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a username and password'],
+  }
 });
 
 UserSchema.pre('save', function(next) {
@@ -15,13 +25,29 @@ UserSchema.pre('save', function(next) {
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
   // Once the password is encrypted, call next() so that your userController and create a user
+  let user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.hash(user.password, SALT_ROUNDS)
+    .then(hash => {
+      user.password = hash;
+      next();
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+UserSchema.methods.checkPassword = function(plainTextPW, cb) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  bcrypt.compare(plainTextPW, this.password)
+    .then(result => {
+      console.log(result);
+      cb(null, result)
+    })
+    .catch(err => cb(err));
 };
 
 // if you're really stuck with this at this point, you can reference this document.
